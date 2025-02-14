8191cc943629355ef0a85952a7460ebdf29ca534
Carter Blackie <carterblackie55@gmail.com>
2025-02-14 10:10:04 -0500

    Getting Started
    Doing some simple code just to make sure the repo is working properly and setting up the branch

---
+++ main.py
@@ -0,0 +1,10 @@
+#Test Program before beggining to code 
+
+#import the random module from library 
+import random
+
+#creating the random number variable
+randomnumber = random.randint(0,50)
+
+#printing the random number to the user
+print("This is a random number:", randomnumber)
